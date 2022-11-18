910711963563f5f47032c6ff30d81c50c290444a
CarinaLSGoulart <carininhalemos@yahoo.com.br>
2022-11-17 22:00:14 -0300

    alteraçoes rotas, controller e middlewares

--- app.js
+++ app.js
@@ -44,4 +44,4 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 /* ===== Error ===== */
 
-app.listen(3000, () => {console.log('Servidor rodando na porta 3000')});
+app.listen(3333, () => {console.log('Servidor rodando na porta 3333')});
